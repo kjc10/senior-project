836b7d5dbb122b46c76cfc3297ecf961c86d013a
kjc10 <kjc10@hood.edu>
2018-03-20 00:13:36 -0400

    First test to upload data

---
+++ scribe_data.sql
@@ -0,0 +1,13 @@
+insert into student values(000398222,'P000398222','Canas Hernandez','Karen','Johanna','90 Waverley Dr. Apt. #O101','Frederick','MD','21702-4815','kjc10@hood.edu','03/28/1995','2404222421','No','kjc10','U','MJBSCS','SR','2015/FALL','12.000','No Athlete','No Visa','Elizabeth Chang','chang@hood.edu',NULL);
+insert into student values(000398223,'P000398223','Denion','Jacob','Wade','Another Address','Frederick','MD','21702-4815','jwd2@hood.edu','03/2/1995','2405552424','No','jwd2','U','MJBSCS','SR','2015/FALL','12.000','No Athlete','No Visa','Elizabeth Chang','chang@hood.edu',NULL);
+
+
+insert into courses values('P000398222','PE 145 03','Yoga','20182','P000106876','Jan','Faulkner','faulkner@hood.edu','faulkner',NULL, 000398222);
+insert into courses values('P000398222','IT 480 01','Web Development Project','20182','P000031525','Elizabeth','Chang','chang@hood.edu','chang', NULL, 000398222);
+insert into courses values('P000398222','CS 335 GD','Teaching Assistantship in CSCI','20182','P000102483','George','Dimitoglou','dimitoglou@hood.edu','dimitoglou',NULL,000398222);
+insert into courses values('P000398223','CS 475 01','Senior Project','20182','P000102483','George','Dimitoglou','dimitoglou@hood.edu','dimitoglou',NULL,000398223);
+insert into courses values('P000398223','CSIT 565 01','Advanced Database Management Systems','20182','P000176844','Aijuan','Dong','dong@hood.edu','dong',NULL,000398223);
+
+insert into notes values('P000398222','PE 145 03', 'new notes', 'notes path', '2018-02-25', '2018-02-26', "Theses are new notes");
+insert into view_notes values('P000398223', 'PE 145 03');
+
